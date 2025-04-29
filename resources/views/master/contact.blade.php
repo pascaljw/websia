50b86cf14f6189a46391c6592df989027cd5f559
pascaljw <akbarfahri047@gmail.com>
2025-04-29 13:26:32 +0800

    mengubah tampilan maps

--- resources/views/master/contact.blade.php
+++ resources/views/master/contact.blade.php
@@ -24,7 +24,7 @@
           <div class="info-item d-flex flex-column justify-content-center align-items-center" data-aos="fade-up" data-aos-delay="200">
             <i class="bi bi-geo-alt"></i>
             <h3>Address</h3>
-            <p>A108 Adam Street, New York, NY 535022</p>
+            <p>Jl.Samratulangi, Sungai Keledang, Kec. Samarinda Seberang, Kota Samarinda</p>
           </div>
         </div><!-- End Info Item -->
 
@@ -48,8 +48,11 @@
 
       <div class="row gy-4 mt-1">
         <div class="col-lg-6" data-aos="fade-up" data-aos-delay="300">
-          <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d48389.78314118045!2d-74.006138!3d40.710059!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25a22a3bda30d%3A0xb89d1fe6bc499443!2sDowntown%20Conference%20Center!5e0!3m2!1sen!2sus!4v1676961268712!5m2!1sen!2sus" frameborder="0" style="border:0; width: 100%; height: 400px;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-        </div><!-- End Google Maps -->
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2857.166724162577!2d117.1209844731069!3d-0.5360303352624535!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2df68080334dac99%3A0x5327a22a4028b267!2sPoliteknik%20Pertanian%20Negeri%20Samarinda!5e1!3m2!1sid!2sid!4v1745903918499!5m2!1sid!2sid" 
+          width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>"
+        </div>
+      </div>
+      <!-- End Google Maps -->
 
         <div class="col-lg-6">
           <form action="forms/contact.php" method="post" class="php-email-form" data-aos="fade-up" data-aos-delay="400">
