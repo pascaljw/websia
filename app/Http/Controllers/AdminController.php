8c5514400a300bf219b37a3b998d30deea02e65e
pascaljw <akbarfahri047@gmail.com>
2025-12-23 10:07:16 +0800

    update dashboard admin

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,12 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Mahasiswa;
+use App\Models\Dosen;
+use App\Models\TugasAkhir;
 
 class AdminController extends Controller
 {
     public function index()
     {
+        $mahasiswaCount = Mahasiswa::count();
+        $dosenCount = Dosen::count();
+        $tugasAkhirCount = TugasAkhir::count();
         
-        return view("admin.index");
+        return view("admin.index", compact('mahasiswaCount', 'dosenCount', 'tugasAkhirCount'));
     }
 }
